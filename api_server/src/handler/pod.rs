11e8cb896dcd40af614bb85ed607eadf0a8d2845
Yuan-Allen <1286362584@qq.com>
2022-04-19 21:29:45 +0800

    feat: update pod phase & validate the kind of object

--- api_server/src/handler/pod.rs
+++ api_server/src/handler/pod.rs
@@ -2,11 +2,11 @@ use std::sync::Arc;
 
 use axum::{extract::Path, http::Uri, Extension, Json};
 use axum_macros::debug_handler;
-use resources::objects::KubeObject;
+use resources::objects::{pod::PodPhase, KubeObject, KubeResource};
 
 use super::{
-    response::{HandlerResult, Response},
-    utils::etcd_put,
+    response::{ErrResponse, HandlerResult, Response},
+    utils::{etcd_put, update_pod_phase},
 };
 use crate::AppState;
 
@@ -14,15 +14,29 @@ use crate::AppState;
 pub async fn apply(
     Extension(app_state): Extension<Arc<AppState>>,
     Path(pod_name): Path<String>,
-    Json(payload): Json<KubeObject>,
+    Json(mut payload): Json<KubeObject>,
     uri: Uri,
 ) -> HandlerResult<()> {
     // TODO: validate payload and add status
-    etcd_put(&app_state, uri.to_string(), payload.clone()).await?;
-    let res = Response::new(format!("pod/{} created", pod_name), None);
-
-    // TODO: fill business logic and error handling
-    let queue = &mut app_state.schedule_queue.write().unwrap();
-    queue.push(payload);
-    Ok(Json(res))
+    // Because we have only one type in KubeResource now,
+    // there will be a warning temporarily.
+    #[allow(irrefutable_let_patterns)]
+    if let KubeResource::Pod(ref mut pod) = payload.resource {
+        update_pod_phase(pod, PodPhase::Pending);
+        etcd_put(&app_state, uri.to_string(), payload.clone()).await?;
+        let res = Response::new(format!("pod/{} created", pod_name), None);
+        let queue = &mut app_state.schedule_queue.write().unwrap();
+        queue.push(payload);
+        Ok(Json(res))
+    } else {
+        // TODO: fill business logic and error handling
+        tracing::debug!("payload kind error");
+        return Err(ErrResponse::new(
+            String::from("apply error"),
+            Some(format!(
+                "the kind of payload: {}, which is not pod",
+                payload.kind()
+            )),
+        ));
+    }
 }
--- api_server/src/handler/utils.rs
+++ api_server/src/handler/utils.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use resources::objects::pod::{Pod, PodPhase, PodStatus};
 use serde::Serialize;
 
 use super::response::ErrResponse;
@@ -15,4 +16,16 @@ pub async fn etcd_put(
         .await
         .map_err(ErrResponse::from)?;
     Ok(())
+}
+
+pub fn update_pod_phase(pod: &mut Pod, new_phase: PodPhase) {
+    if let Some(ref mut status) = pod.status {
+        status.phase = new_phase;
+    } else {
+        let new_status: PodStatus = PodStatus {
+            phase: new_phase,
+            ..PodStatus::default()
+        };
+        pod.status = Some(new_status);
+    }
 }
