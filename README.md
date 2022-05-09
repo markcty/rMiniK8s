132f68d1ba38d319b92af17467fa3d1ab00426d4
markcty <markcty@outlook.com>
2022-05-09 11:23:27 +0800

    feat: enable cni plugin on node

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # minik-8-s/rminik8s
 
-
-
 ## Getting Started
 
 Download links:
@@ -10,21 +8,39 @@ SSH clone URL: ssh://git@git.jetbrains.space/minik8s/minik-8-s/rminik8s.git
 
 HTTPS clone URL: https://git.jetbrains.space/minik8s/minik-8-s/rminik8s.git
 
-
-
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+These instructions will get you a copy of the project up and running on your local machine for development and testing
+purposes.
 
 ## Prerequisites
 
 What things you need to install the software and how to install them.
 
 ```
-Examples
+docker
 ```
 
 ## Deployment
 
-Add additional notes about how to deploy this on a production system.
+### Node
+
+在部署node之前需要保证ETCD中写入以下规则：
+
+```shell
+etcdctl set /coreos.com/network/config '{ "Network": "10.5.0.0/16", "Backend": {"Type": "vxlan"}}'
+```
+
+建议使用multipass起虚拟机来测试，启动带docker的multipass：
+
+```shell
+multipass launch docker -n k8s1
+multipass shell k8s1
+```
+
+在虚拟机中运行以下命令部署node，其间会要求输入配置：
+
+```shell
+source <(curl -s https://s3.jcloud.sjtu.edu.cn/1b088ff214b04e6291c549a95685610b-share/deploy.sh)
+```
 
 ## Resources
 
---
+++ scripts/node/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# stop docker
+systemctl stop docker.service containerd.service docker.socket
+
+# env
+ARCH=$(dpkg --print-architecture)
+echo "ARCH: $ARCH"
+
+# install flannel
+if [ ! -f /usr/local/bin/flanneld ]; then
+  echo "Installing flanneld ..."
+  wget -q --show-progress -O /usr/local/bin/flanneld "https://github.com/flannel-io/flannel/releases/download/v0.17.0/flanneld-${ARCH}"
+  chmod +x /usr/local/bin/flanneld
+fi
+
+# install services
+echo "Installing systemd services ..."
+wget -q --show-progress -O /etc/systemd/system/flanneld.service https://s3.jcloud.sjtu.edu.cn/1b088ff214b04e6291c549a95685610b-share/flanneld.service
+wget -q --show-progress -O /etc/systemd/system/kdocker.service https://s3.jcloud.sjtu.edu.cn/1b088ff214b04e6291c549a95685610b-share/kdocker.service
+
+# write configuration file
+if [ ! -f /etc/rminik8s/conf.env ]; then
+  echo "Configuration not found. Generating..."
+  echo -n "Please input master etcd endpoint(eg. http://127.0.0.1:2379): "
+  read -r ETCD_ENDPOINT
+  mkdir -p /etc/rminik8s
+  printf 'ETCD_ENDPOINT="%s"\n' "$ETCD_ENDPOINT" > /etc/rminik8s/conf.env
+fi
+
+# start
+systemctl daemon-reload
+systemctl restart flanneld.service
+echo "flanneld started"
+
+while [ ! -f /run/flannel/subnet.env ]; do
+  sleep 1
+done
+systemctl restart kdocker.service
+echo "docker started"
