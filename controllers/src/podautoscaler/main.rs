d7cd3ed4270831bdb3f22c0810e433acf848735b
yyoung <yyoung2001@gmail.com>
2022-05-19 16:22:44 +0800

    feat: horizontal pod autoscaler

---
+++ controllers/src/podautoscaler/main.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate lazy_static;
+
+use anyhow::{Context, Result};
+use config::{Config, File};
+use resources::config::ClusterConfig;
+
+mod horizontal;
+mod metrics;
+mod replica_calculator;
+mod utils;
+
+lazy_static! {
+    pub static ref CONFIG: ClusterConfig = Config::builder()
+        .add_source(File::with_name("/etc/rminik8s/controller-manager.yaml"))
+        .build()
+        .unwrap_or_default()
+        .try_deserialize::<ClusterConfig>()
+        .with_context(|| "Failed to parse config".to_string())
+        .unwrap_or_default();
+}
+
+/// Default sync period in seconds
+pub static SYNC_PERIOD: u32 = 15;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let mut controller = horizontal::PodAutoscaler::new();
+    controller.run().await?;
+    Ok(())
+}
