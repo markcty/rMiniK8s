a04c16b22cc6ea7d0c7f4c3d50b2b27dc5d9cd69
markcty <markcty@outlook.com>
2022-06-01 22:55:28 +0800

    feat: add pull images script

---
+++ scripts/arm/pull_images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker pull minik8s.xyz/api_server-arm:latest
+docker pull minik8s.xyz/endpoints-controller-arm:latest
+docker pull minik8s.xyz/ingress-controller-arm:latest
+docker pull minik8s.xyz/podautoscaler-arm:latest
+docker pull minik8s.xyz/replicaset-controller-arm:latest
+docker pull minik8s.xyz/scheduler-arm:latest
+docker pull minik8s.xyz/gpujob-controller-arm:latest
+docker pull minik8s.xyz/serverless-router-arm:latest
+docker pull minik8s.xyz/function-controller-arm:latest
