db4f467872364ae51534029969d802f6a15f628e
markcty <markcty@outlook.com>
2022-06-01 23:26:52 +0800

    feat: add different capacity vm

--- scripts/multipass/init.sh
+++ scripts/multipass/init.sh
@@ -5,11 +5,10 @@ if [ -z "$RMINIK8S" ]; then
 fi
 
 # master
-multipass launch -n k8s -d 20G --cloud-init cloud-init-arm64.yaml 20.04
-
-# node
-multipass launch -n k8s1 -d 20G --cloud-init cloud-init-arm64.yaml 20.04
-multipass launch -n k8s2 -d 20G --cloud-init cloud-init-arm64.yaml 20.04
+multipass launch --name k8s --disk 20G --cpus 4 --mem 4G --cloud-init cloud-init-arm64.yaml lts
+# 2 nodes: 2v8g, 6v4g
+multipass launch --name k8s1 --disk 20G --cpus 2 --mem 8G --cloud-init cloud-init-arm64.yaml lts
+multipass launch --name k8s2 --disk 20G --cpus 6 --mem 4G --cloud-init cloud-init-arm64.yaml lts
 
 multipass mount $RMINIK8S k8s:~/rminik8s
 multipass mount $RMINIK8S k8s1:~/rminik8s
